d202da6dbdb9da6937187617dc8428ad1d9a43e9
polyakov-artem <polyakov.contact@yandex.ru>
2024-09-18 10:27:23 +0300

    build: add .prettierrc config, prettier, eslint-config-prettier, eslint-plugin-prettier. Change eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -4,11 +4,16 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import reactCompiler from "eslint-plugin-react-compiler";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default tseslint.config(
   { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      eslintPluginPrettierRecommended,
+    ],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,

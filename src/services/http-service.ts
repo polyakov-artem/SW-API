128333bad0957bc8186ccc058255574b41a4bfd4
polyakov-artem <polyakov.contact@yandex.ru>
2024-11-10 14:23:58 +0300

    feat: add httpService

---
+++ src/services/http-service.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://swapi.dev/api/';
+
+const http = axios.create({
+  baseURL: BASE_URL,
+});
+
+const httpService = {
+  get: http.get,
+  post: http.post,
+  put: http.put,
+  delete: http.delete,
+  patch: http.patch,
+};
+
+export default httpService;

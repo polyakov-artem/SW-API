5f56f40a2e96488646f7c491f856726a968a5ff4
polyakov-artem <polyakov.contact@yandex.ru>
2024-11-10 11:35:12 +0300

    feat: add InputChangeHandlerType,  SelectChangeHandlerType, SubmitHandlerType

--- src/types/types.tsx
+++ src/types/types.tsx
@@ -1,1 +1,7 @@
+import { ComponentProps } from 'react';
+
 export type Size = 'xs' | 'sm' | 'md' | 'lg' | 'xlg';
+
+export type InputChangeHandlerType = NonNullable<ComponentProps<'input'>['onChange']>;
+export type SelectChangeHandlerType = NonNullable<ComponentProps<'select'>['onChange']>;
+export type SubmitHandlerType = NonNullable<ComponentProps<'form'>['onSubmit']>;

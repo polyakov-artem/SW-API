b9ed0188f9ad1487d0f35e7141e93604aef43736
polyakov-artem <polyakov.contact@yandex.ru>
2024-11-12 19:38:00 +0300

    feat: add loadData helper function

---
+++ src/utils/load-data.tsx
@@ -0,0 +1,70 @@
+import { isAxiosError } from 'axios';
+
+export const DEFAULT_MESSAGE = 'Unexpected error';
+export const CANCELED_REQUEST_MESSAGE = 'Request was canceled';
+export const NO_DATA_MESSAGE = 'No data was received from the server';
+
+export type IdleState = { status: LoadingStatus.idle; data: null; error: null };
+export type LoadingState = { status: LoadingStatus.loading; data: null; error: null };
+export type ErrorState = { status: LoadingStatus.error; data: null; error: string };
+
+export type SuccessState<DataType> = {
+  status: LoadingStatus.success;
+  data: DataType;
+  error: null;
+};
+
+export type LoaderState<DataType> = IdleState | SuccessState<DataType> | ErrorState | LoadingState;
+
+export enum LoadingStatus {
+  idle = 'idle',
+  loading = 'loading',
+  success = 'success',
+  error = 'error',
+}
+
+export function getIdleState(): IdleState {
+  return { status: LoadingStatus.idle, data: null, error: null };
+}
+
+export function getLoadingState(): LoadingState {
+  return { status: LoadingStatus.loading, data: null, error: null };
+}
+
+export function getSuccessState<DataType>(data: DataType): SuccessState<DataType> {
+  return {
+    status: LoadingStatus.success,
+    data,
+    error: null,
+  };
+}
+
+export function getErrorState(error: string): ErrorState {
+  return { status: LoadingStatus.error, data: null, error };
+}
+
+export async function loadData<DataType>(
+  requestFunction: () => Promise<DataType>
+): Promise<SuccessState<DataType> | ErrorState> {
+  let fetchState;
+
+  try {
+    const data = await requestFunction();
+    if (!data) throw new Error(NO_DATA_MESSAGE);
+    fetchState = getSuccessState(data);
+  } catch (e) {
+    let message = DEFAULT_MESSAGE;
+
+    if (e instanceof Error) {
+      if (isAxiosError(e) && e.code === 'ERR_CANCELED') {
+        message = CANCELED_REQUEST_MESSAGE;
+      } else {
+        message = e.message;
+      }
+    }
+
+    fetchState = getErrorState(message);
+  }
+
+  return fetchState;
+}

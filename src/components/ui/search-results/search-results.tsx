162c1fd974ae3d2d62799af63de6578a5a593e71
polyakov-artem <polyakov.contact@yandex.ru>
2024-11-16 21:37:42 +0300

    fix: fix App to cancel requests, prevent data change in unmounted state or on canceled request

--- src/components/ui/search-results/search-results.tsx
+++ src/components/ui/search-results/search-results.tsx
@@ -3,14 +3,23 @@ import Loader from '../../shared/loader/loader';
 import './search-results.scss';
 import { LoaderState } from '../../../utils/load-data';
 import { CorrectResponseDataType } from '../../../types/types';
+import { SwCategory } from '../../../enums/enums';
+import { capitalizeWord } from '../../../utils/capitalize-word';
 
-const SearchResults: FC<LoaderState<CorrectResponseDataType>> = ({ data, status, error }) => {
+type SearchResultsPropsType = LoaderState<CorrectResponseDataType> & { category: SwCategory };
+
+const SearchResults: FC<SearchResultsPropsType> = ({ data, status, error, category }) => {
   const content = ((status) => {
     const cases = {
       idle: null,
       loading: <Loader className="search-results__loader" />,
       error: <h2>Error occurred while loading: {error}</h2>,
-      success: data?.count ? JSON.stringify(data) : <h2>No items was found</h2>,
+      success:
+        data?.count && data.results ? (
+          JSON.stringify(data.results)
+        ) : (
+          <h2>No items was found in category "{capitalizeWord(category)}"</h2>
+        ),
     };
 
     return cases[status];

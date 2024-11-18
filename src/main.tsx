b253943705ae543640ce23d1c524887794fa414f
polyakov-artem <polyakov.contact@yandex.ru>
2024-11-18 11:52:23 +0300

    feat: add RouterProvider, routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
 import './scss/index.scss';
-import { BrowserRouter } from 'react-router-dom';
-import ErrorBoundary from './components/shared/error-boundary/error-boundary.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import routes from './routes';
+
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ErrorBoundary>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ErrorBoundary>
+    <RouterProvider router={router} />
   </StrictMode>
 );

71c2aefb60a17d3eaf9ad792e21b8807b6b70b4f
polyakov-artem <polyakov.contact@yandex.ru>
2024-11-18 13:53:32 +0300

    feat: extract items search logic from Search to useCategoryLoader hook

--- src/components/ui/search/search.tsx
+++ src/components/ui/search/search.tsx
@@ -1,69 +1,24 @@
-import { FC, useCallback, useEffect, useRef, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 import SearchHeader from '../search-header/search-header';
 import SearchResults from '../search-results/search-results';
 import { Outlet } from 'react-router';
 import { selectOptions } from '../../../constants/constants';
-import {
-  CANCELED_REQUEST_MESSAGE,
-  getLoadingState,
-  loadData,
-  LoaderState,
-} from '../../../utils/load-data';
-import { CorrectResponseDataType, ItemsSearchQueryType } from '../../../types/types';
+import { ItemsSearchQueryType } from '../../../types/types';
 import useParamsForItemsSearch from '../../../hooks/use-params-for-items-search';
-import swService from '../../../services/sw-service';
-
-type CategoryLoaderStateType = LoaderState<CorrectResponseDataType>;
+import useCategoryLoader from '../../../hooks/use-items-search';
 
 const Search: FC = () => {
   const paramsForItemsSearch = useParamsForItemsSearch();
-  const [categoryLoader, setCategoryLoader] = useState<CategoryLoaderStateType>(getLoadingState);
   const [searchQuery, setSearchQuery] = useState<ItemsSearchQueryType>(paramsForItemsSearch);
+  const categoryLoader = useCategoryLoader(searchQuery);
 
   const handleSubmit = () => {
     setSearchQuery({ ...searchQuery });
   };
 
-  const abortRef = useRef(new AbortController());
-  const isMountedRef = useRef(false);
-
-  const searchItems = useCallback(async ({ search, category, page }: ItemsSearchQueryType) => {
-    abortRef.current?.abort();
-    const controller = new AbortController();
-    abortRef.current = controller;
-
-    setCategoryLoader(getLoadingState);
-
-    const nextLoaderState = await loadData(() =>
-      swService.search({
-        search,
-        category,
-        page,
-        signal: controller.signal,
-      })
-    );
-
-    if (!isMountedRef.current || nextLoaderState.error === CANCELED_REQUEST_MESSAGE) return;
-
-    setCategoryLoader(nextLoaderState);
-  }, []);
-
   useEffect(() => {
     setSearchQuery(paramsForItemsSearch);
   }, [paramsForItemsSearch]);
-
-  useEffect(() => {
-    searchItems(searchQuery);
-  }, [searchItems, searchQuery]);
-
-  useEffect(() => {
-    isMountedRef.current = true;
-
-    return () => {
-      isMountedRef.current = false;
-      abortRef.current?.abort();
-    };
-  }, []);
 
   const { search, category } = searchQuery;
 

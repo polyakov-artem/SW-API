53b8fa0406a65c536db95eff5988a40b6f81fa18
polyakov-artem <polyakov.contact@yandex.ru>
2024-11-22 11:34:43 +0300

    feat: add NotFoundPage to routes, add NotFoundPage, NotFoundBanner

---
+++ src/pages/not-found-page/not-found-page.tsx
@@ -0,0 +1,10 @@
+import NotFoundBanner from '../../components/shared/not-found-banner/not-found-banner';
+import './not-found-page.scss';
+
+export default function NotFoundPage() {
+  return (
+    <main className="not-found-page container">
+      <NotFoundBanner />
+    </main>
+  );
+}

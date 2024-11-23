431546ea51220c17f9240b4a35899cf5b65e4c6c
polyakov-artem <polyakov.contact@yandex.ru>
2024-11-23 10:34:12 +0300

    build: add msw server, add msw server stop and start for all tests

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,7 +1,20 @@
 import '@testing-library/jest-dom';
 import { afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
+import { server } from './msw/server';
 
 afterEach(() => {
   cleanup();
+});
+
+beforeAll(() => {
+  server.listen();
+});
+
+afterEach(() => {
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  server.close();
 });

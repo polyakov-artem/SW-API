e5ad7dabf314ef35ffd571748c3d0e5def01238a
polyakov-artem <polyakov.contact@yandex.ru>
2024-11-23 10:34:50 +0300

    build: add msw utils

---
+++ tests/msw/msw-utils.ts
@@ -0,0 +1,18 @@
+import { delay, http, HttpResponse } from 'msw';
+import { SetupServerApi } from 'msw/node';
+
+export const addServerDelay = (server: SetupServerApi, url: string = '*') => {
+  server.use(
+    http.get(url, async () => {
+      await delay('infinite');
+    })
+  );
+};
+
+export const addNetworkError = (server: SetupServerApi, url: string = '*') => {
+  server.use(
+    http.get(url, async () => {
+      return HttpResponse.error();
+    })
+  );
+};

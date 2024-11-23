b3f03a0308cb24da9de8950ba876d851c2a5fde7
polyakov-artem <polyakov.contact@yandex.ru>
2024-11-23 11:16:35 +0300

    feat: add test utils

---
+++ tests/utils/test-utils.ts
@@ -0,0 +1,45 @@
+export type getter = () => HTMLElement[] | HTMLElement;
+
+export const assertExistance = (...getters: Array<getter>) => {
+  if (getters.length === 0) throw new Error('Should pass at least one getter');
+
+  const elements = {} as Record<string, HTMLElement[] | HTMLElement>;
+
+  getters.forEach((getter) => {
+    if (!/^get\w/.test(getter.name)) {
+      throw new Error("Getter function name should start with 'get'");
+    }
+
+    const name = getter.name.replace(/^get(\w)/, (_, firstLetter) => {
+      return firstLetter.toLowerCase();
+    });
+
+    elements[name] = getter();
+    expect(elements[name]).toBeInTheDocument();
+  });
+
+  return elements;
+};
+
+export const assertAbsence = (...getters: Array<getter>) => {
+  if (getters.length === 0) throw new Error('Should pass at least one getter');
+
+  getters.forEach((getter) => expect(getter).toThrow());
+};
+
+export const assertElements = (params: { exist: getter[]; absent: getter[] }) => {
+  const elements = assertExistance(...params.exist);
+  assertAbsence(...params.absent);
+
+  return elements;
+};
+
+export const toRegExp = (text: unknown, flags: string = 'i') => new RegExp(String(text), flags);
+
+export const delay = (time?: number) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('resolved');
+    }, time);
+  });
+};

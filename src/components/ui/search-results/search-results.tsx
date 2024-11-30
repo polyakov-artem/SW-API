37300bd09a61e97ec3af64dfdbb8727934d27cc4
polyakov-artem <polyakov.contact@yandex.ru>
2024-11-30 13:46:44 +0300

    feat: add Pagination, change SearchResults to add Pagination

--- src/components/ui/search-results/search-results.tsx
+++ src/components/ui/search-results/search-results.tsx
@@ -1,16 +1,31 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import Loader from '../../shared/loader/loader';
 import './search-results.scss';
-import { LoaderState } from '../../../utils/load-data';
+import { LoaderState, LoadingStatus } from '../../../utils/load-data';
 import { CorrectResponseDataType, ItemsSearchQueryType } from '../../../types/types';
 import { capitalizeWord } from '../../../utils/capitalize-word';
 import ResultsList from '../results-list/results-list';
+import Pagination from '../../shared/pagination/pagination';
 
 type SearchResultsPropsType = LoaderState<CorrectResponseDataType> & {
   searchQuery: ItemsSearchQueryType;
 };
 
 const SearchResults: FC<SearchResultsPropsType> = ({ data, status, error, searchQuery }) => {
+  const [prevData, setPrevData] = useState(data);
+  const [prevCategory, setPrevCategory] = useState(searchQuery.category);
+
+  useEffect(() => {
+    if (searchQuery.category !== prevCategory) {
+      setPrevCategory(searchQuery.category);
+      setPrevData(data);
+      return;
+    }
+    if (status === LoadingStatus.success) {
+      setPrevData(data);
+    }
+  }, [searchQuery, prevCategory, data, status]);
+
   const content = ((status) => {
     const cases = {
       idle: null,
@@ -27,7 +42,18 @@ const SearchResults: FC<SearchResultsPropsType> = ({ data, status, error, search
     return cases[status];
   })(status);
 
-  return <div className="search-results">{content}</div>;
+  return (
+    <div className="search-results">
+      <Pagination
+        className="search-results__pagination"
+        totalCount={prevData?.count}
+        perPageCount={10}
+        currentPageParam={searchQuery.page}
+        numOfVisibleButtons={7}
+      />
+      <div className="search-results__list-container">{content}</div>
+    </div>
+  );
 };
 
 export default SearchResults;

c5754531fe8c6caf890fd0954bce441d4cb61a94
polyakov-artem <polyakov.contact@yandex.ru>
2024-11-30 11:29:10 +0300

    fix: change Button props, remove ButtonInner

--- src/components/shared/button/button.tsx
+++ src/components/shared/button/button.tsx
@@ -1,73 +1,37 @@
 import './button.scss';
 import { ComponentProps, FC, ReactNode } from 'react';
-import { Size } from '../../../types/types';
 import { getClassNames } from '../../../utils/getClassNames';
+import { ClassModsType } from '../../../types/types';
 
 export const BASE_CLASS_NAME = 'btn';
 export const btnIconClassName = `${BASE_CLASS_NAME}__icon`;
 export const btnInnerClassName = `${BASE_CLASS_NAME}__inner`;
 export const btnTextClassName = `${BASE_CLASS_NAME}__text`;
 
-export interface ButtonPropsType {
-  disabled?: boolean;
-  view?: string;
-  theme?: string;
-  size?: Size;
-  iconBefore?: boolean;
-  className?: string;
-  children?: ReactNode;
-  state?: 'idle' | 'loading';
+export interface ButtonPropsType extends ComponentProps<'button'> {
+  classMods?: ClassModsType & {
+    'icon-before'?: boolean;
+    view?: 'primary';
+  };
   icon?: ReactNode;
-  loadingIcon?: ReactNode;
 }
 
-type ButtonComponentPropsType = ButtonPropsType & ComponentProps<'button'>;
-type ButtonInnerPropsType = { icon?: ReactNode; children?: ReactNode };
-
-const Button: FC<ButtonComponentPropsType> = (props) => {
-  const {
-    disabled,
-    view = 'primary',
-    theme,
-    size,
-    iconBefore = false,
-    className,
-    children,
-    state = 'idle',
-    icon,
-    loadingIcon,
-    ...intrinsicProps
-  } = props;
+const Button: FC<ButtonPropsType> = (props) => {
+  const { disabled, classMods, className, icon, children, ...btnIntrinsicProps } = props;
 
   const classes = getClassNames({
     baseClass: BASE_CLASS_NAME,
-    classMods: {
-      view,
-      theme,
-      size,
-      disabled,
-      'icon-before': iconBefore,
-    },
+    classMods,
     mix: className,
   });
 
-  const currentIcon = state === 'idle' ? icon : loadingIcon;
-
   return (
-    <button className={classes} disabled={disabled} {...intrinsicProps}>
-      <ButtonInner icon={currentIcon}>{children}</ButtonInner>
+    <button className={classes} disabled={disabled} {...btnIntrinsicProps}>
+      <span className={btnInnerClassName}>
+        <span className={btnTextClassName}>{children}</span>
+        {icon && <span className={btnIconClassName}>{icon}</span>}
+      </span>
     </button>
-  );
-};
-
-export const ButtonInner = (props: ButtonInnerPropsType) => {
-  const { children, icon } = props;
-
-  return (
-    <span className={btnInnerClassName}>
-      <span className={btnTextClassName}>{children}</span>
-      {icon && <span className={btnIconClassName}>{icon}</span>}
-    </span>
   );
 };
 

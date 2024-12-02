f27f4a7bce3345c2cb6bbddb4b921b6391b8e6d4
polyakov-artem <polyakov.contact@yandex.ru>
2024-12-02 16:32:44 +0300

    fix: change getBaseUrlForPageParam implementation

--- src/components/shared/pagination/pagination-utils.ts
+++ src/components/shared/pagination/pagination-utils.ts
@@ -1,7 +1,24 @@
 export const getBaseUrlForPageParam = (url: string) => {
-  const clearedPath = url.replace(/\?(page=.*&)|(&page=[^&]*)/i, '');
-  const hasEndSlash = /\/$/.test(clearedPath);
-  const hasParams = /\/\?/.test(clearedPath);
+  const hasParams = /\?/.test(url);
+  const hasMultipleParams = /&/.test(url);
   const pageParamString = 'page=';
-  return `${clearedPath}${hasParams ? `&${pageParamString}` : hasEndSlash ? `?${pageParamString}` : `/?${pageParamString}`}`;
+
+  if (!hasParams) {
+    return `${url}?${pageParamString}`;
+  }
+
+  const firstParamRegexp = /(?<=\?)page=\w*&*/gi;
+
+  if (hasMultipleParams) {
+    const middleOrEndParamRegexp = /&page=\w*/gi;
+
+    if (firstParamRegexp.test(url) || middleOrEndParamRegexp.test(url)) {
+      return (
+        url.replace(firstParamRegexp, '').replace(middleOrEndParamRegexp, '') +
+        `&${pageParamString}`
+      );
+    }
+  }
+
+  return url.replace(firstParamRegexp, '') + `${pageParamString}`;
 };

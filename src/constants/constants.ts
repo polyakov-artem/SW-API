39d04b9a4a42f46ecc3b96bcc045aa2ea38883e3
polyakov-artem <polyakov.contact@yandex.ru>
2024-12-07 09:47:18 +0300

    build: add BASE_URL to constants, add 'base: BASE_URL' setting to vite.config.ts

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,6 +1,8 @@
 import { SwCategory } from '../enums/enums';
 import { capitalizeWord } from '../utils/capitalize-word';
 
+export const BASE_URL = '/SW-API/';
+
 export const selectOptions = Object.values(SwCategory).map((category) => ({
   label: capitalizeWord(category),
   value: category,
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { BASE_URL } from './src/constants/constants';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: BASE_URL,
 });

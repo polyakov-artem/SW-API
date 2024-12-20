328f66a7641cfc0acf7aceb7abc2604c3be65320
polyakov-artem <polyakov.contact@yandex.ru>
2024-12-20 09:42:15 +0300

    fix: change server URL

--- src/services/http-service.ts
+++ src/services/http-service.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BASE_URL = 'https://swapi.dev/api/';
+export const BASE_URL = 'https://swapi-api.hbtn.io/api/';
 
 const http = axios.create({
   baseURL: BASE_URL,

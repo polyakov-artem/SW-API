1f4f3b9616c739da056d7b3feb2d7bd23bf7ea29
polyakov-artem <polyakov.contact@yandex.ru>
2025-01-13 10:52:22 +0300

    fix: fix routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import ItemDetails from './components/ui/item-details/item-details';
 import NotFoundPage from './pages/not-found-page/not-found-page';
 import { PUBLIC_PATH } from './constants/constants';
+import { SwCategory } from './enums/enums';
 
 const appWithErrorBoundary = (
   <ErrorBoundary>
@@ -11,20 +12,33 @@ const appWithErrorBoundary = (
   </ErrorBoundary>
 );
 
+const generateCategoriesRoutes = (publicPath: string, categories: string[]) => {
+  return categories.map((category) => {
+    return {
+      path: `${publicPath}${category}`,
+      element: appWithErrorBoundary,
+      children: [
+        {
+          path: ':itemId',
+          element: <ItemDetails />,
+        },
+      ],
+    };
+  });
+};
+
 const routes: RouteObject[] = [
   {
-    path: `${PUBLIC_PATH}:category?`,
+    path: `${PUBLIC_PATH}`,
     element: appWithErrorBoundary,
-
-    children: [
-      {
-        path: ':itemId',
-        element: <ItemDetails />,
-      },
-    ],
   },
+  ...generateCategoriesRoutes(PUBLIC_PATH, Object.values(SwCategory)),
   {
     path: `${PUBLIC_PATH}not-found-page`,
+    element: <NotFoundPage />,
+  },
+  {
+    path: `*`,
     element: <NotFoundPage />,
   },
 ];

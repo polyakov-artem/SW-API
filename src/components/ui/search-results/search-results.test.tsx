41b7ad2bb17b48b74749033373f27f80c421c153
polyakov-artem <polyakov.contact@yandex.ru>
2025-01-19 16:24:21 +0300

    test: fix SearchResults test

--- src/components/ui/search-results/search-results.test.tsx
+++ src/components/ui/search-results/search-results.test.tsx
@@ -1,12 +1,18 @@
 import { getAllByRole, getByRole, screen } from '@testing-library/react';
 import { assertAbsence, assertExistance, createGetter } from '../../../../tests/utils/test-utils';
-import { TCategoryLoaderAndSearchQuery } from '../../../types/types';
 import SearchResults from './search-results';
 import { SwCategory } from '../../../enums/enums';
 import { responses } from '../../../../tests/mocks/constants';
-import { getLoadingState, getSuccessState } from '../../../utils/load-data';
 import { PUBLIC_PATH } from '../../../constants/constants';
 import { renderWithRouter } from '../../../utils/test/render-with-router';
+import itemsLoaderReducer, {
+  initialState as initialItemsLoaderState,
+  itemsLoadingFinished,
+} from '../../../store/items-loader-slice';
+import downloadLoaderReducer from '../../../store/download-items-slice';
+import { Provider } from 'react-redux';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { act } from 'react';
 import { Route, Routes } from 'react-router';
 
 const getSearchResults = createGetter('search-results');
@@ -17,43 +23,40 @@ const getResultsList = () => getByRole(getContentWrap(), 'list');
 const getContentLoader = () => screen.getByTestId('loader');
 const getCurrentLocation = () => window.location.toString();
 
-const firstProps = {
-  searchQuery: {
-    category: SwCategory.people,
-    search: 'a',
-    page: '1',
-  },
-  categoryLoader: getSuccessState(responses.notEmptySearch.category.people[0]),
+const endpointsResponses = {
+  [SwCategory.people]: responses.emptySearch.category.people[0],
+  [SwCategory.films]: responses.emptySearch.category.films[0],
 };
 
-const secondProps = {
-  searchQuery: {
-    category: SwCategory.films,
-    search: 'a',
-    page: '1',
-  },
-  categoryLoader: getSuccessState(responses.notEmptySearch.category.films[1]),
-};
+const renderSearchResults = (category: SwCategory, itemsLoaderState = initialItemsLoaderState) => {
+  const store = configureStore({
+    preloadedState: { itemsLoader: itemsLoaderState },
+    reducer: combineReducers({
+      itemsLoader: itemsLoaderReducer,
+      downloadItems: downloadLoaderReducer,
+    }),
+  });
 
-const renderSearchResults = (props: TCategoryLoaderAndSearchQuery = firstProps) => {
-  const { category, search, page } = props.searchQuery;
-
-  return renderWithRouter(
-    <>
-      <Routes>
-        <Route path={`${PUBLIC_PATH}:category/:itemId?`} element={<SearchResults {...props} />} />
-      </Routes>
-    </>,
-    {
-      route: `${PUBLIC_PATH}${category}/1/?search=${search}&page=${page}`,
-    }
-  );
+  return {
+    getState: store.getState,
+    dispatch: store.dispatch,
+    ...renderWithRouter(
+      <Provider store={store}>
+        <Routes>
+          <Route path={`${PUBLIC_PATH}${category}/:itemId?`} element={<SearchResults />} />
+        </Routes>
+      </Provider>,
+      {
+        route: `${PUBLIC_PATH}${category}/1/?search=a&page=1`,
+      }
+    ),
+  };
 };
 
 describe('SearchResults', () => {
-  describe('when data are loading', () => {
+  describe('when no data or data are loading', () => {
     test('should render correctly', () => {
-      renderSearchResults({ ...firstProps, categoryLoader: getLoadingState() });
+      renderSearchResults(SwCategory.films, { ...initialItemsLoaderState, isLoading: true });
 
       assertExistance(getSearchResults, getPaginationWrap, getContentWrap, getContentLoader);
       assertAbsence(getPagination);
@@ -61,10 +64,14 @@ describe('SearchResults', () => {
   });
 
   describe('when data are loaded', () => {
-    test('should render pagination with correct buttons, correctly render results list', () => {
-      const props = firstProps;
-      const { data } = props.categoryLoader;
-      renderSearchResults(props);
+    test('should render pagination with correct buttons, correctly render results list', async () => {
+      const category = SwCategory.people;
+      const data = endpointsResponses[category];
+
+      renderSearchResults(category, {
+        ...initialItemsLoaderState,
+        data,
+      });
 
       const paginationItems = getAllByRole(getPagination(), 'listitem');
       expect(paginationItems.map((link) => link.textContent)).toEqual([
@@ -74,41 +81,46 @@ describe('SearchResults', () => {
         '3',
         '4',
         '5',
-        '6',
+        '...',
+        '9',
         '»',
       ]);
 
       const resultsListItems = getAllByRole(getResultsList(), 'listitem');
-      const titles = data.results.map((result) => result.name);
+      const names = data.results.map((result) => result.name);
       resultsListItems.forEach((listItem) =>
-        expect(titles.some((title) => listItem.textContent === title)).toBe(true)
+        expect(names.some((name) => listItem.textContent === name)).toBe(true)
       );
     });
   });
 
   describe('when data are changed', () => {
     test('should update pagination', async () => {
-      const { rerender } = renderSearchResults();
-
+      const category = SwCategory.people;
+      const data = endpointsResponses[category];
+      const { dispatch } = renderSearchResults(category, {
+        ...initialItemsLoaderState,
+        data,
+      });
       const paginationItems = getAllByRole(getPagination(), 'listitem');
-      expect(paginationItems.length).toBe(8);
+      expect(paginationItems.length).toBe(9);
 
-      const newProps = secondProps;
-      const { category, search, page } = newProps.searchQuery;
-      const route = `${PUBLIC_PATH}${category}/1/?search=${search}&page=${page}`;
+      act(() => {
+        dispatch(itemsLoadingFinished(endpointsResponses[SwCategory.films]));
+      });
 
-      rerender(<SearchResults {...secondProps} />, { route });
       assertAbsence(getPagination);
     });
   });
 
   describe('when clicking outside pagination and content areas', () => {
     test('should navigate to the correct URL', async () => {
-      const { user } = renderSearchResults();
+      const category = SwCategory.people;
+      const { user } = renderSearchResults(category);
+
       await user.click(getSearchResults());
 
       const { search } = window.location;
-      const { category } = firstProps.searchQuery;
       const expectedRoute = `http://localhost:3000${PUBLIC_PATH}${category}/${search}`;
       expect(getCurrentLocation()).toBe(expectedRoute);
     });
@@ -116,7 +128,8 @@ describe('SearchResults', () => {
 
   describe('when clicking inside pagination wrap or content wrap', () => {
     test('should not navigate', async () => {
-      const { user } = renderSearchResults();
+      const category = SwCategory.people;
+      const { user } = renderSearchResults(category);
       const savedURL = getCurrentLocation();
 
       await user.click(getPaginationWrap());
